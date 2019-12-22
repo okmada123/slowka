c6fc1e41f63233b9a8d975962ab0cc8a35ff8bb9
jkloris <kapusta.jergus@gmail.com>
2019-12-22 20:35:22 +0100

    pridana funkcia factorial

--- permutacie.c
+++ permutacie.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#define DLZKA_SLOVA 6
-#define MAX_PERM 120
+#define DLZKA_SLOVA 4
+#define MAX_PERM 24
 int test = 1;
 
 void perm(char fix[], char str[], char pole_slov[][DLZKA_SLOVA]) {
@@ -13,27 +13,31 @@ void perm(char fix[], char str[], char pole_slov[][DLZKA_SLOVA]) {
 		slovo[i + 2] = '\0';
 		slovo[i] = str[0];
 		slovo[i + 1] = str[1];
+
+
 		//start
-;
 		for (o = 0; o < strlen(slovo); o++) {
 			pole_slov[test - 1][o] = slovo[o];
 		}
 		pole_slov[test - 1][o] = '\0';
-		printf("%s %d\n", pole_slov[test - 1],test-1);
-
+		//printf("%s %d\n", pole_slov[test - 1],test-1);
 		//end
+
+
 		printf("%d: %s\n", test++, slovo);
 		slovo[i] = str[1];
 		slovo[i + 1] = str[0];
-		//start
 
+
+		//start
 		for (o = 0; o < strlen(slovo); o++) {
 			pole_slov[test - 1][o] = slovo[o];
 		}
 		pole_slov[test - 1][o] = '\0';
-		printf("%s %d\n", pole_slov[test - 1], test - 1);
-
+		//printf("%s %d\n", pole_slov[test - 1], test - 1);
 		//end
+
+
 		printf("%d: %s\n", test++, slovo);
 
 		return;
@@ -58,13 +62,21 @@ void perm(char fix[], char str[], char pole_slov[][DLZKA_SLOVA]) {
 	}
 }
 
+int fact(int n) {
+	if (n == 1) return 1;
+	else {
+		n *= fact(n - 1);
+		return n;
+	}
+}
+
 
 
 int main() {
 	char str[DLZKA_SLOVA + 1], fix[DLZKA_SLOVA + 1] = "";
 	//neviem jak dostat const. do toho scanf...
 	printf("Zadaj pismena, z ktorych chces skladat permutacie (bez medzery): ");
-	scanf("%5s", str);
+	scanf("%4s", str);
 	
 	// start upravy
 	char pole_slov[MAX_PERM+1][DLZKA_SLOVA];
@@ -78,6 +90,9 @@ int main() {
 	for (i = 0; i < MAX_PERM; i++) {
 		printf("%s\n", pole_slov[i]);
 	}
+
+	int n = fact(5);
+	printf("%d", n);
 	//end
 	
 	return 0;
