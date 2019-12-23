31c947705c0299cc498b5cc54b3bd57f5a71e8c6
jkloris <kapusta.jergus@gmail.com>
2019-12-23 11:00:41 +0100

    pridana funkcia, ktora prepise do noveho pola len neopakujuce sa slova

--- permutacie.c
+++ permutacie.c
@@ -84,6 +84,29 @@ int chars_repeat(char *str) {
 	return 0;
 }
 
+int vymaz_opakovane(char** pole_slov, char** pole_slovNEW, int n) {
+	int i,o;
+	
+
+	for (i = 0; i < n-1; i++) {
+		for (o = i+1; o < n; o++) {
+			if (!strcmp(pole_slov[i], pole_slov[o])) {
+				//printf("Zhoda %s%d = %s%d \n",pole_slov[i],i+1,pole_slov[o],o+1);
+				pole_slov[o][0] = '*';
+			}
+		}
+
+	}
+		
+	for (i = 0, o = 0; i < n; i++) {
+		if (pole_slov[i][0] != '*') {
+			strcat(pole_slovNEW[o++], pole_slov[i]);
+			//printf("nove pole %s\n", pole_slovNEW[i]);
+		}
+	}
+	return o;
+}
+
 
 
 int main() {
@@ -96,10 +119,14 @@ int main() {
 	int n = fact(strlen(str));
 
 	char **pole_slov = (char**)calloc(n, sizeof(char*));					//alokovanie pamate ..
+	char **pole_slovNEW = (char**)calloc(n, sizeof(char*));					//.
 	int i;																	//..
 	for (i = 0; i < n; i++) {
-		pole_slov[i] = (char*)calloc((strlen(str) ),sizeof(char) );			//..pre 2D dynamicke pole
+		pole_slov[i] = (char*)calloc((strlen(str) ),sizeof(char) );			//..
+		pole_slovNEW[i] = (char*)calloc((strlen(str)), sizeof(char));		//..pre 2D dynamicke pole
 	}
+
+	
 	//end
 
 
@@ -109,13 +136,26 @@ int main() {
 	//start
 	if (chars_repeat(str)) {
 		printf("Opakuju sa\n");
+		int velkost_novehoP = vymaz_opakovane(pole_slov,pole_slovNEW,n);
+		
+		for (i = 0; i < velkost_novehoP; i++) {
+			printf("%s\n", pole_slovNEW[i]);
+		}
+		/*while (pole_slovNEW[i] != '\0') {
+
+		}*/
+	}
+	else
+	{
+		for (i = 0; i < n; i++) {
+			printf("%s\n", pole_slov[i]);
+		}
 	}
 
-	for (i = 0; i < n; i++) {
-		printf("%s\n",	pole_slov[i]);
-	}
+	
 
 	free(pole_slov);
+	free(pole_slovNEW);
 	//end
 	
 	return 0;
